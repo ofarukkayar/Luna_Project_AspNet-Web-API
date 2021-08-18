f858c27a5fd49db6585814bfad79967597117468
DESKTOP-BB8KMG3\khadgar <faruk.kayar@ug.bilkent.edu.tr>
2021-08-18 11:05:11 +0300

    Add project files.

---
+++ Luna_Project_AspNet-Web-API.Core/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Luna_Project_AspNet_Web_API.Core.Models
+{
+    class Category
+    {
+        public Category()
+        {
+            Products = new Collection<Product>();
+        }
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
---
+++ Luna_Project_AspNet-Web-API.Core/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Luna_Project_AspNet_Web_API.Core.Models
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+
+        public virtual Category Category { get; set; }
+
+    }
+}
